e3737b0f183b76fdce739b5d3f1bf00ec0c15e2e
naveen <naveenmanchanda1995@gmail.com>
2020-01-15 23:11:08 +0530

    added redirection on click

--- script.js
+++ script.js
@@ -6,25 +6,30 @@ fetch(req).then(function(response) {
     return response.text();
 }).then(function(text) {
     var news = JSON.parse(text).articles
-    // console.log(news);
-    for (var i = 0; i < news.length; i++) {
+
+    for (let i = 0; i < news.length; i++) {
         var news_head = document.createElement("h3");
         var news_image = document.createElement("IMG");
         var news_desc = document.createElement("p")
         var news_card = document.createElement("div")
         news_card.className = "w3-panel w3-card-4"
+        news_card.id = i;
         news_image.setAttribute("src", news[i].urlToImage);
         news_image.setAttribute("width", "310");
         news_image.setAttribute("height", "232");
         news_image.setAttribute("alt", "The Pulpit Rock");
-        console.log(news[i])
         news_card.appendChild(news_head)
         news_card.appendChild(news_image);
         news_card.appendChild(news_desc);
         document.getElementById("title").appendChild(news_card)
+
         news_head.innerHTML = news[i].title
         news_desc.innerHTML = news[i].description
+        document.getElementById(i).onclick = function() {
+            location.href = news[i].url;
 
+        }
 
     }
+
 });
